5708df09ed1fa41804e6948749147980279399e1
Agnik Sarkar <121817949+agniksarkar1107@users.noreply.github.com>
2024-02-21 00:06:45 +0530

    Update auto_eda.py

--- auto_eda.py
+++ auto_eda.py
@@ -9,7 +9,7 @@ hide_default_format = """
 
 from PIL import Image
 
-im = Image.open('/Users/agniksarkar/Automated_EDA/business-analytics-icon-data-analysis-illustration-sign-data-science-symbol-profit-graph-logo-vector.jpg')
+im = Image.open('business-analytics-icon-data-analysis-illustration-sign-data-science-symbol-profit-graph-logo-vector.jpg')
 
 st.set_page_config( page_title="Automated EDA App", page_icon = im ,layout="wide")
 st.markdown(hide_default_format, unsafe_allow_html=True)
